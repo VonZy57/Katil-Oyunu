ef0519e74089fc8a74478c17b6b6b3a84f801f64
Mirzacob <Mrzcoban@gmail.com>
2026-03-16 21:39:54 +0300

    radnom generate öncesi save

---
+++ Assets/Scripts/MiniGames/EndlessRunner/TempLamp.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class TempLamp : MonoBehaviour
+{
+    public Material matOn;
+    public Material matOff;
+    public float interval = 0.5f;
+
+    private Renderer _renderer;
+    private float _timer;
+    private bool _isOn = true;
+
+    void Start()
+    {
+        _renderer = GetComponentInChildren<Renderer>();
+        _renderer.material = matOn;
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= interval)
+        {
+            _timer = 0f;
+            _isOn = !_isOn;
+            _renderer.material = _isOn ? matOn : matOff;
+        }
+    }
+}
