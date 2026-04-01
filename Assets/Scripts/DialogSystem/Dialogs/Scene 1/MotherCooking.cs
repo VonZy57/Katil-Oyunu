da0a6e71fabffcdde00c33a0322807a6f688c29f
Yunus <winneryunus57@gmail.com>
2026-04-01 18:55:03 +0300

    Görev Scriptleri

--- Assets/Scripts/DialogSystem/Dialogs/Scene 1/MotherCooking.cs
+++ Assets/Scripts/DialogSystem/Dialogs/Scene 1/MotherCooking.cs
@@ -13,4 +13,27 @@ public class MotherCooking : MonoBehaviour
     {
         
     }
+
+    void BuildDialogTree()
+    {
+        DialogNode firstPart = DialogBuilder.CreateNode
+        ("I sh*t on neighbor's bed, I sh*t on neighbor's bed, I,I thougt he loved me, I thought he loved me!",
+        "Komşunun yatağına s*çtım, Komşunun yatağına s*çtım, Beni, beni seviyor sandım, Beni seviyor sandım!",
+        "Anne");
+
+        DialogNode secondPart = DialogBuilder.CreateNode
+        ("I made beans in the pot, I made tzatziki to go with, I put pickles next to it, I left my son an orphan, I left my son an orphan!",
+        "Ocağa fasulye attım attım, Yanına cacık yaptım, Yanına turşu koydum, Oğlumu yetim koydum, Oğlumu yetim koydum!",
+        "Anne");
+
+        DialogNode thirdPart = DialogBuilder.CreateNode
+        ("I sh*t on my neighbor's bed, I put tzatziki next to it, My son is my darling, I won't let you get sl*tty woman!",
+        "Komşunun yatağına sıçtım, yanına cacık koydum, Oğlum benim canım, Salmam sana sana k*şar kadın kadın",
+        "Anne");
+
+        DialogNode finalPart = DialogBuilder.CreateNode
+        ("I left my son an orphan, I f*cked my neighbor's p*ssy",
+        "Oğlumu yetim koydum, Komşumun a*ına koydum.",
+        "Anne");
+    }
 }
