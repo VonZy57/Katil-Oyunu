a83e42ee66a2482995b31f70f33b9b3d0083da96
MrLtexe <mrzcoban@gmail.com>
2026-06-05 17:15:00 +0300

    a

--- Assets/Scripts/TriggerBoxes/CheckOutSideTrigger.cs
+++ Assets/Scripts/TriggerBoxes/CheckOutSideTrigger.cs
@@ -1,22 +1,52 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
-public class CheckOutSideTrigger : MonoBehaviour
+public class CheckOutSideTrigger : Interactable
 {
+    [Header("Geçiş Ayarları")]
+    public Image fadeImage;
+    public float fadeDuration = 2f;
+
+    private bool isTransitioning = false;
+
+    void Start()
+    {
+        promptMessage = "E - Exit";
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-            EndScene();
+            StartTransition();
     }
 
-    void EndScene()
+    protected override void Interact()
     {
-        MissionObjective missionObj = GetComponent<MissionObjective>();
-        //Sonraki g�revi tetikler ve sahne de�i�tirir
-        if (missionObj != null)
-            missionObj.OnInteracted();
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartTransition();
     }
 
+    void StartTransition()
+    {
+        if (isTransitioning) return;
+        isTransitioning = true;
+
+        GetComponent<MissionObjective>()?.OnInteracted();
+
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            Color color = fadeImage.color;
+            color.a = 0f;
+            fadeImage.color = color;
+
+            fadeImage.DOFade(1f, fadeDuration).OnComplete(() =>
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
 }
